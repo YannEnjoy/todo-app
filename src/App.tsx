f955d842a3e80415b3999fb5ae5cdbe6b5664a75
sugiyoshi <sugiyoshi.tech@gmail.com>
2024-06-20 23:10:12 +0900

    implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { TodoList } from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
+import { TodoSummary } from './components/TodoSummary';
+
+import { useTodoList } from './hooks/useTodoList';
+
 function App() {
+  const { todoList, changeCompleted, addTodo, deleteTodo, deleteAllCompleted } =
+    useTodoList();
+
   return (
-    <main className="mx-auto mt-10 max-w-xl">
+    <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
+      <div className="space-y-5">
+        <AddTodoForm addTodo={addTodo} />
+        <div className="space-y-5 rounded bg-slate-200 p-5">
+          <TodoList
+            todoList={todoList}
+            changeCompleted={changeCompleted}
+            deleteTodo={deleteTodo}
+          />
+          <TodoSummary deleteAllCompleted={deleteAllCompleted} />
+        </div>
+      </div>
     </main>
   );
 }
